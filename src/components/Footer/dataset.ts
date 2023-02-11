197c38e1a6845ef5ca9945a639d803fe83c8d495
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-11 00:44:52 -0300

    Footer

---
+++ src/components/Footer/dataset.ts
@@ -0,0 +1,80 @@
+const socialmedia = [
+  {
+    href: 'https://www.linkedin.com/in/eduardo-fertig-bastos/',
+    icon: 'uil uil-linkedin',
+  },
+  {
+    href: 'https://github.com/EduardoFertigBastos',
+    icon: 'uil uil-github',
+  }
+];
+
+const dataset = {
+  en: {
+    menu: [
+      {
+        href: '#home',
+        icon: 'uil uil-estate',
+        text: 'Home',
+      },
+      {
+        href: '#about',
+        icon: 'uil uil-user',
+        text: 'About',
+      },
+      {
+        href: '#portfolio',
+        icon: 'uil uil-scenery',
+        text: 'Portfolio',
+      },
+      {
+        href: '#skills',
+        icon: 'uil uil-file-alt',
+        text: 'Skills',
+      },
+      {
+        href: '#contact',
+        icon: 'uil uil-file-alt',
+        text: 'Contact',
+      }
+    ],
+    name: `Eduardo Fertig Bastos`,
+    subtitle: `Full Stack Developer`,
+    socialmedia
+  },
+  pt: {
+    menu: [
+      {
+        href: '#home',
+        icon: 'uil uil-estate',
+        text: 'Home',
+      },
+      {
+        href: '#about',
+        icon: 'uil uil-user',
+        text: 'Sobre',
+      },
+      {
+        href: '#portfolio',
+        icon: 'uil uil-scenery',
+        text: 'Portfólio',
+      },
+      {
+        href: '#skills',
+        icon: 'uil uil-file-alt',
+        text: 'Habilidades',
+      },
+      {
+        href: '#contact',
+        icon: 'uil uil-file-alt',
+        text: 'Contato',
+      }
+    ],
+    
+    name: `Eduardo Fertig Bastos`,
+    subtitle: `Desenvolvedor Full Stack`,
+    socialmedia
+  }
+}
+
+export default dataset;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback } from 'react';
+
+import { useLanguage } from 'hooks/useLanguage';
+
+import dataset from './dataset';
+
+const Footer: React.FC = () => {
+
+  const lang = useLanguage();
+  const data = dataset[lang];
+
+  return (
+    <footer className="footer">
+      <div className="footer__bg">
+        <div className="footer__container container grid">
+          <div>
+            <span className="footer__title">{data.name}</span>
+            <br />
+            <span className="footer__subtitle">{data.subtitle}</span>
+          </div>
+
+          <ul className="footer__links">
+            {
+              data.menu.map((item, index) => (
+                <li key={index}>
+                  <a href={item.href} className="footer__link">{item.text}</a>
+                </li>
+              ))
+            }
+          </ul>
+
+          <div className="footer__socials">
+            {
+              data.socialmedia.map((item, index) => (
+                <a key={index} 
+                  href={item.href} 
+                  target="_blank" 
+                  className="footer__social" 
+                  rel="noreferrer"
+                >
+                  <i className={item.icon}></i>
+                </a>
+              ))
+            }
+          </div>
+
+        </div>
+        <p className="footer__copy">
+          Based on <a href="https://github.com/bedimcode/responsive-portfolio-website-Alexa" 
+            target="_blank" rel="noreferrer"
+          > Responsive Personal Website Alexa </a> 
+          by <a href=" https://github.com/bedimcode" target="_blank"> Bedimcode </a>
+          <br />
+          Copyright &#169; Eduardo Fertig Bastos. All rights reserved.
+            </p>
+        </div>
+    </footer>
+  )
+};
+
+export default Footer;
