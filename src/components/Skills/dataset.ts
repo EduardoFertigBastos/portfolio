e44d23d24af8fc4a744e4ebacfc7ee1d9e000e1e
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-11 00:44:30 -0300

    Skills

---
+++ src/components/Skills/dataset.ts
@@ -0,0 +1,170 @@
+
+const dataset = {
+  en: {
+    title: 'Skills',
+    subtitle: 'A little bit on my technical level',
+    skills: [
+      {
+        id: 1,
+        title: 'Development',
+        subtitle: 'Programming languages and databases',
+        icon: 'uil uil-brackets-curly',
+        list: [
+          {
+            name: 'Javascript',
+            level: '100%',
+          },
+          {
+            name: 'Typescript',
+            level: '100%',
+          },
+          {
+            name: 'PHP',
+            level: '100%',
+          },
+          {
+            name: 'HTML5',
+            level: '100%',
+          },
+          {
+            name: 'CSS3',
+            level: '100%',
+          },
+          {
+            name: 'PostgreSQL',
+            level: '100%',
+          },
+          {
+            name: 'MongoDB',
+            level: '80%',
+          },
+        ]
+      },
+      {
+        id: 2,
+        title: 'Frameworks & APIs',
+        subtitle: 'My favorite frameworks and APIs',
+        icon: 'uil uil-server-network',
+        list: [
+          {
+            name: 'React JS',
+            level: '100%',
+          },
+          {
+            name: 'Node.js',
+            level: '100%',
+          },
+          {
+            name: 'Laravel',
+            level: '100%',
+          },
+          {
+            name: 'Angular',
+            level: '50%',
+          },
+          {
+            name: 'JQuery',
+            level: '100%',
+          },
+          {
+            name: 'React Native',
+            level: '60%',
+          },
+          {
+            name: 'Bootstrap',
+            level: '100%',
+          },
+          {
+            name: 'Material UI',
+            level: '80%',
+          },
+        ]
+      },
+    ]
+  },
+  pt: {
+    title: 'Habilidades',
+    subtitle: 'Um pouco sobre minhas habilidades técnicas',
+    skills: [
+      {
+        id: 1,
+        title: 'Desenvolvimento',
+        subtitle: 'Linguagens de programação e bancos de dados',
+        icon: 'uil uil-brackets-curly',
+        list: [
+          {
+            name: 'Javascript',
+            level: '100%',
+          },
+          {
+            name: 'Typescript',
+            level: '100%',
+          },
+          {
+            name: 'PHP',
+            level: '100%',
+          },
+          {
+            name: 'HTML5',
+            level: '100%',
+          },
+          {
+            name: 'CSS3',
+            level: '100%',
+          },
+          {
+            name: 'PostgreSQL',
+            level: '100%',
+          },
+          {
+            name: 'MongoDB',
+            level: '80%',
+          },
+        ]
+      },
+      {
+        id: 2,
+        title: 'Frameworks & APIs',
+        subtitle: 'Minhas frameworks e APIs preferidas',
+        icon: 'uil uil-server-network',
+        list: [
+          {
+            name: 'React JS',
+            level: '100%',
+          },
+          {
+            name: 'Node.js',
+            level: '100%',
+          },
+          {
+            name: 'Laravel',
+            level: '100%',
+          },
+          {
+            name: 'Angular',
+            level: '50%',
+          },
+          {
+            name: 'JQuery',
+            level: '100%',
+          },
+          {
+            name: 'React Native',
+            level: '60%',
+          },
+          {
+            name: 'Bootstrap',
+            level: '100%',
+          },
+          {
+            name: 'Material UI',
+            level: '80%',
+          },
+        ]
+      },
+    ]
+  },
+
+}
+
+export default dataset;
---
+++ src/components/Skills/index.tsx
@@ -0,0 +1,76 @@
+import React, { useCallback, useState } from 'react';
+
+import { useLanguage } from 'hooks/useLanguage';
+
+import dataset from './dataset';
+
+const Skills: React.FC = () => {
+
+  const lang = useLanguage();
+  const data = dataset[lang];
+
+  const [actives, setActives] = useState<number[]>([]);
+
+  const handleStatus = useCallback((id: number) => {
+    if (actives.includes(id)) {
+      return setActives(actives.filter((active) => active !== id));
+    } 
+    
+    setActives([...actives, id]);
+  }, [actives]);
+
+  return (
+    <section className="skills section" id="skills">
+      <h2 className="section__title">{ data.title }</h2>
+      <span className="section__subtitle">{ data.subtitle }</span>
+
+      <div className="skills__container container grid">
+          {
+            data.skills.map((skill, index1) => (
+
+              // <div className="skills__content skills__close">
+
+              <div key={index1} className={
+                actives.includes(skill.id) 
+                  ? 'skills__content skills__open'
+                  : 'skills__content skills__close'
+                }
+                onClick={() => handleStatus(skill.id)}
+              >
+                <div className="skills__header">
+                  <i className={`skills__icon ${skill.icon}`}></i>
+
+                  <div>
+                    <h1 className="skills__title">{skill.title}</h1>
+                    <span className="skills__subtitle">{skill.subtitle}</span>
+                  </div>
+
+                  <i className="uil uil-angle-down skills__arrow"></i>
+                </div>
+
+                <div className="skills__list grid">
+                  {
+                    skill.list.map((item, index2) => (
+                      <div key={index2} className="skills__data">
+                        <div className="skills__titles">
+                          <h3 className="skills__name">{item.name}</h3>
+                          <span className="skills__number">{item.level}</span>
+                        </div>
+                        <div className="skills__bar">
+                          <span className="skills__percentage skills__50"></span>
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            
+            ))
+          }
+
+        </div>
+    </section>
+  )
+};
+
+export default Skills;
