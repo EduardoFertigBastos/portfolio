784d1868065de12961271122af1f5c3f1be1576b
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-14 00:17:27 -0300

    Modal Projects

---
+++ src/components/Portfolio/ModalProject/index.tsx
@@ -0,0 +1,117 @@
+import React, { useCallback } from "react";
+
+import Modal from "react-modal";
+
+import ImageSlider from "../ImageSlider";
+import Project from "../interface/Project";
+
+import {
+	Body,
+	CloseButton,
+	DescriptionSection,
+	Image,
+	MainSection,
+	PhotoSection,
+	SeparateBar,
+	Skill,
+	SkillSection,
+} from "./styles";
+
+const customStyles = {
+	overlay: {
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundColor: "rgba(32, 29, 29, 0.75)",
+	},
+	content: {
+		top: "50%",
+		left: "50%",
+		right: "auto",
+		bottom: "auto",
+
+		width: "80%",
+		height: "80%",
+		borderRadius: "36px",
+		marginRight: "-50%",
+		transform: "translate(-50%, -50%)",
+	},
+};
+
+
+
+interface Props {
+	isOpen: boolean;
+	setIsOpen: (isOpen: boolean) => void;
+	project?: Project;
+}
+
+Modal.setAppElement("#root");
+
+const ModalProject: React.FC<Props> = ({ isOpen, setIsOpen, project }) => {
+	function closeModal() {
+		setIsOpen(false);
+	}
+
+	const handleImageClick = useCallback(
+		(e: React.MouseEvent<HTMLImageElement>) => {
+      const galleryField = document.querySelector<HTMLImageElement>(
+				"#gallery-field"
+			);
+			
+      if (!galleryField) {
+				return
+			}
+
+			galleryField.style.display = 'block';
+			galleryField.src = e.currentTarget.src;
+			galleryField.alt = e.currentTarget.alt;
+		},
+    []
+	);
+
+	return (
+		<Modal
+			isOpen={isOpen}
+			style={customStyles}
+			contentLabel='Example Modal'
+			onRequestClose={closeModal}
+			shouldCloseOnOverlayClick={true}
+			shouldCloseOnEsc={true}>
+			<CloseButton>
+				<button onClick={closeModal}>X</button>
+			</CloseButton>
+			<Body>
+				<MainSection>
+					<PhotoSection>
+						<Image >
+							<ImageSlider slides={project?.images as string[]} title={project?.title as string}/>
+						</Image>
+					</PhotoSection>
+
+          <SeparateBar />
+
+					<DescriptionSection>
+						<h2>{project?.title}</h2>
+						{
+							project?.description.split('\n\n').map((paragraph, index) => (
+								<p key={index}>{paragraph}</p>
+							))
+						}
+					</DescriptionSection>
+				</MainSection>
+				<SkillSection>
+					{project?.skills.map(({ icon: Icon, name }) => (
+						<Skill key={name}>
+							{<Icon />}
+							<span>{name}</span>
+						</Skill>
+					))}
+				</SkillSection>
+			</Body>
+		</Modal>
+	);
+};
+
+export default ModalProject;
---
+++ src/components/Portfolio/ModalProject/styles.ts
@@ -0,0 +1,129 @@
+import styled from 'styled-components';
+
+export const CloseButton = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  button {
+    background-color: transparent;
+    border: none;
+    font-size: 25px;
+    cursor: pointer;
+  }
+`;
+
+export const Body = styled.div`
+  justify-content: center;
+  align-items: center;
+  font-size: 1.7rem;
+  text-align: center;
+  width: 100%;
+  height: 94%;
+
+  @media screen and (max-width: 1200px) {
+    display: block;
+  }
+`;
+
+export const MainSection = styled.section`
+  width: 100%;
+  height: 85%;
+  display: flex;
+`
+
+export const DescriptionSection = styled.section`
+  width: 50%;
+  height: 100%;
+  margin-right: 10px;
+  
+  h2 {
+    font-size: var(--h1-font-size);
+    margin-bottom: var(--mb-0-5);
+  }
+
+  p {
+    margin-bottom: var(--mb-0-75);
+    text-align: justify;
+    font-size: 1.2rem;
+  }
+  
+`
+export const SeparateBar = styled.section`
+  width: 2px;
+  height: 23%;
+  margin: auto 30px;
+  background-color: var(--first-color);
+
+  @media screen and (max-width: 1200px) {
+    width: 0;
+    height: 0;
+    margin: 5px auto;
+  }
+`;
+
+export const SkillSection = styled.section`
+  width: 103%;
+  margin-left:-25px;
+  
+  height: 140px;
+  background: hsl(var(--hue-color), 28%, 12%);
+  padding: 0 10%; 
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+`
+
+export const Skill = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  transition: all 0.4s;
+
+  svg {
+    font-size: 3.8rem;
+    display: block;
+    margin: 0 auto;
+  }
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`
+
+export const PhotoSection = styled.section`
+  width: 48%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-right: 10px;
+  margin-top: -14px;
+
+`
+
+export const Image = styled.div`
+  min-width: 400px;
+  justify-content: center;
+`
+
+export const GalleryPhotoSection = styled.div`
+  padding-top: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+export const ImgGallery = styled.img`
+  border: 1px solid #ddd; 
+  border-radius: 4px;  
+  padding: 5px; 
+  width: 150px; 
+  margin: 0 3px;
+  
+  max-height: 150px; 
+  object-fit: cover;
+`;
