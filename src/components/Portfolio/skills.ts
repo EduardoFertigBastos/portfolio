8e335fd06d8174c9fe3aded50a30682d618536b6
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-14 00:17:44 -0300

    Create skills.ts

---
+++ src/components/Portfolio/skills.ts
@@ -0,0 +1,79 @@
+import { BsFillBootstrapFill } from 'react-icons/bs'
+import { FaLaravel, FaNodeJs, FaPhp, FaReact } from 'react-icons/fa'
+import {
+  SiCss3,
+  SiHtml5,
+  SiJavascript,
+  SiJquery,
+  SiMaterialui,
+  SiPostgresql,
+  SiTypescript
+} from 'react-icons/si'
+
+import Skill from './interface/Skill'
+
+
+export const bootstrap: Skill = {
+  name: 'Bootstrap',
+  icon: BsFillBootstrapFill
+}
+
+export const css: Skill = {
+  name: 'CSS 3',
+  icon: SiCss3
+}
+
+export const html: Skill = {
+  name: 'HTML 5',
+  icon: SiHtml5
+}
+
+export const javascript: Skill = {
+  name: 'Javacript',
+  icon: SiJavascript
+}
+
+export const jquery: Skill = {
+  name: 'JQuery',
+  icon: SiJquery
+}
+
+export const laravel: Skill = {
+  name: 'Laravel',
+  icon: FaLaravel
+}
+
+export const materialui: Skill = {
+  name: 'Material UI',
+  icon: SiMaterialui
+}
+
+export const node: Skill = {
+  name: 'Node.js',
+  icon: FaNodeJs
+}
+
+export const php: Skill = {
+  name: 'PHP',
+  icon: FaPhp
+}
+
+export const postgresql: Skill = {
+  name: 'PostgreSQL',
+  icon: SiPostgresql
+}
+
+export const react: Skill = {
+  name: 'React JS',
+  icon: FaReact
+}
+
+export const reactNative: Skill = {
+  name: 'React Native',
+  icon: FaReact
+}
+
+export const typescript: Skill = {
+  name: 'Typescript',
+  icon: SiTypescript
+}
