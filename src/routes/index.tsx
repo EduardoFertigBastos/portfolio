3a3ec53ef2425fa66cc055435e26736b4a09b9b4
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-14 20:42:53 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ import Home from '../pages/Home';
 const Routes: React.FC = () => (
   <SwitchRoutes>
     <Route path="/" element={<Home />} />
+    <Route path="/portfolio" element={<Home />} />
   </SwitchRoutes>
 );
 
