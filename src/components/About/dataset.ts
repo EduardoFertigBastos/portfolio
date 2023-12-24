4e9823683e93432739e3de79ca887da943b0ca23
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2023-12-24 14:57:41 -0300

    Update dataset.ts

--- src/components/About/dataset.ts
+++ src/components/About/dataset.ts
@@ -5,12 +5,12 @@ const dataset = {
     title: 'About Me',
     subtitle: 'A brief introduction',
     img: 'assets/img/about.jpg',
-    description: `Hello, I am Eduardo Fertig Bastos and I am a Full-Stack Developer. `
-          + `I have 5 years of experience. I graduated from the UNIDAVI information systems `
-          + `course, which I iniatialize on 2019 and finished in 2022.  `
-          + `To work with back-endI am more familiarized with Node.js, Laravel and PHP vanilla. `
-          + `To work with front-end I am more familiarized with React JS, JavaScript vanilla and JQuery. `
-          + `On databases I worked with PostgreSQL, MySQL, SQLite, SQL Server and MongoDB. `,
+    description: `
+    Hello! I'm Eduardo Fertig Bastos, a Full-Stack developer with 5 years of experience and a degree in Information Systems from UNIDAVI (2019-2022).\n\n
+    
+    In the front-end realm, I feel right at home using Next.js and React JS, but I've also had considerable experience with Vue 3, Angular, vanilla JavaScript, and JQuery. When it comes to the back-end, I find my comfort zone with Node.js, Laravel, and, not least, vanilla PHP. Regarding databases, my preference leans towards PostgreSQL, but I've also gained significant experience with MySQL, SQLite, SQL Server, and MongoDB.\n\n
+    
+    Beyond the lines of code, I believe in the power of soft skills. I advocate for effective communication, teamwork, and I highly value empathy. With a sprinkle of constant curiosity, I am eager to contribute to projects that truly make a difference.`,
     description2: 'If you want to know more about me, feel free to contact me.',
     info: [
       {
@@ -35,7 +35,11 @@ const dataset = {
     title: 'Sobre mim',
     subtitle: 'Uma breve introdução',
     img: 'assets/img/about.jpg',
-    description: `Olá, eu sou Eduardo Fertig Bastos e sou um desenvolvedor Full-Stack. Tenho 5 anos de experiência e sou formado em Sistemas de Informação pela UNIDAVI, curso que iniciei em 2019 e finalizei em 2022. Para trabalhar com back-end, estou mais familiarizado com Node.js, Laravel e PHP vanilla. Em relação às ferramentas para o front-end, prefiro utilizar React JS, porém também tenho experiência com JavaScript vanilla e JQuery. No que se refere a bancos de dados, tenho utilizado principalmente PostgreSQL, mas também possuo experiência com MySQL, SQLite, SQL Server e MongoDB.`,
+    description: `Olá! Eu sou Eduardo Fertig Bastos, um desenvolvedor Full-Stack com 5 anos de experiência e formação em Sistemas de Informação pela UNIDAVI (2019-2022).\n\n
+
+    Na área do front-end, me sinto em casa trabalhando com Next.js e React JS, mas já acumulei valiosa experiência com Vue 3, Angular, JavaScript vanilla e JQuery. No back-end, minha expertise se destaca em Node.js, Laravel e, não menos importante, PHP vanilla. Quando o assunto é banco de dados, minha preferência recai sobre o PostgreSQL, mas também tenho sólida experiência com MySQL, SQLite, SQL Server e MongoDB.\n\n
+    
+    Para mim, o desenvolvimento vai além do código. Acredito no poder das soft skills. Sou defensor da comunicação efetiva, trabalho em equipe e prezo pela empatia. Com uma dose constante de curiosidade, estou entusiasmado para contribuir em projetos que realmente façam a diferença.`,
     description2: 'Caso queira saber mais sobre mim, sinta-se livre para entrar em contato.',
     info: [
       {
